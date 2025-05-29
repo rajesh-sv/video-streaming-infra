605dc193dc699f9c7c96646e0a0344214f226e7e
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2025-05-29 22:28:50 +0530

    Add logout component

---
+++ frontend/src/components/logout.tsx
@@ -0,0 +1,17 @@
+import { Button } from "@/components/ui/button";
+import useLogout from "@/hooks/useLogout";
+import { RoundSpinner } from "@/components/ui/spinner";
+
+export function Logout() {
+  const { loading, logout } = useLogout();
+
+  return (
+    <Button
+      className="cursor-pointer bg-red-300 hover:bg-red-500 hover:text-white"
+      disabled={loading}
+      onClick={logout}
+    >
+      {loading ? <RoundSpinner /> : "Logout"}
+    </Button>
+  );
+}
