e590daecd52114e0b9665a38882172464c9055d5
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2025-05-31 14:14:16 +0530

    Add aws environment variables

--- backend/upload-service/src/config/config.js
+++ backend/upload-service/src/config/config.js
@@ -1,8 +1,12 @@
-const { NODE_ENV } = process.env;
+const { NODE_ENV, S3_BUCKET, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY } =
+  process.env;
 
 const PORT = process.env.PORT || 80;
 
 return {
   NODE_ENV,
+  BUCKET,
   PORT,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
 };
