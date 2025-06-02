a3956d836f3cb12d66cda656449ce37050d52936
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2025-06-02 19:16:08 +0530

    Add express server

---
+++ backend/search-service/src/server.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import { searchRouter } from "./routes/searchRoutes.js";
+import { logger } from "./utils/pinoLogger.js";
+import { PORT } from "./config/config.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(helmet());
+app.use(searchRouter);
+
+app.listen(PORT, () => {
+  logger.info(`Server running on port: ${PORT}`);
+});
+
+const errorTypes = ["unhandledRejection", "uncaughtException"];
+const signalTraps = ["SIGTERM", "SIGINT", "SIGUSR2"];
+
+errorTypes.forEach((type) => {
+  process.on(type, (e) => {
+    try {
+      logger.fatal(type);
+      logger.error(e);
+      process.exit(0);
+    } catch (_) {
+      logger.error("Error during exit, forcing exit...");
+      process.exit(1);
+    }
+  });
+});
+
+signalTraps.forEach((type) => {
+  process.once(type, () => {
+    logger.fatal(type);
+    logger.info("Killing the process");
+    process.kill(process.pid, type);
+  });
+});
