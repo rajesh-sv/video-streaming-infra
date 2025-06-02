eea4fe015c1e039345873f4827b1dad8e1f2c627
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2025-06-02 11:23:50 +0530

    Add kafka producer

---
+++ backend/upload-service/src/db/kafkaProducer.js
@@ -0,0 +1,25 @@
+import { Kafka } from "kafkajs";
+import { KAFKA_BROKER } from "../config/config.js";
+
+const kafka = new Kafka({
+  clientId: "video-upload-service",
+  brokers: [KAFKA_BROKER, "localhost:9092"],
+});
+
+const producer = kafka.producer();
+await producer.connect();
+
+export function sendMessages(topic, message) {
+  return producer.send({
+    topic,
+    messages: [
+      {
+        value: message,
+      },
+    ],
+  });
+}
+
+export function endConnection() {
+  return producer.disconnect();
+}
