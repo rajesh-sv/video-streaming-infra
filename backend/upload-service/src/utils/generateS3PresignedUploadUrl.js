53e4129e918006a38fb932a6b762751901530aa3
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2025-06-02 11:26:24 +0530

    Refactor to pass key to function

--- backend/upload-service/src/utils/generateS3PresignedUploadUrl.js
+++ backend/upload-service/src/utils/generateS3PresignedUploadUrl.js
@@ -1,24 +1,24 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
-import { v4 as uuid4 } from "uuid";
 import {
+  REGION,
   S3_BUCKET,
   AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY,
 } from "../config/config.js";
 
 const s3Client = new S3Client({
-  region: "ap-south-1",
+  region: REGION,
   credentials: {
     accessKeyId: AWS_ACCESS_KEY_ID,
     secretAccessKey: AWS_SECRET_ACCESS_KEY,
   },
 });
 
-export async function generateS3PresigneduploadUrl(filename) {
+export async function generateS3PresignedUploadUrl(key) {
   const command = new PutObjectCommand({
     Bucket: S3_BUCKET,
-    key: `${uuid4()}-${filename}`,
+    Key: key,
   });
 
   const uploadUrl = await getSignedUrl(s3Client, command, { expiresIn: 3600 });
