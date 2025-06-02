227a1f8d5a6623f900c6e0f3780b53346d5cc6f1
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2025-06-02 11:21:42 +0530

    Add user authenication middleware

--- backend/upload-service/src/routes/uploadRoutes.js
+++ backend/upload-service/src/routes/uploadRoutes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import {
   getS3PresignedUploadUrl,
   postVideo,
-} from "../controllers/uploadController";
+} from "../controllers/uploadController.js";
+import { authenticateUser } from "../middlewares/authenticateUser.js";
 
 const uploadRouter = Router();
 
-uploadRouter.get("/s3UploadUrl", getS3PresignedUploadUrl);
+uploadRouter.post("/s3UploadUrl", authenticateUser, getS3PresignedUploadUrl);
 
-uploadRouter.post("/video", postVideo);
+uploadRouter.post("/video", authenticateUser, postVideo);
 
 export { uploadRouter };
