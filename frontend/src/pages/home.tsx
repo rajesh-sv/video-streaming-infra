7a269b64422f1af872f90f9893a7a6d6f8ae1373
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2025-06-05 09:38:43 +0530

    Add video search functionality

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,14 +1,50 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Link } from "react-router";
+import { Search } from "lucide-react";
+import { RoundSpinner } from "@/components/ui/spinner";
+import { useSearch } from "@/hooks/useSearch";
+import { useState } from "react";
 
 export function Home() {
+  const [searchString, setSearchString] = useState("");
+  const { loading, videos, search } = useSearch();
+
   return (
-    <div className="flex gap-8">
-      <Input type="text" placeholder="Search..." className="max-w-sm" />
-      <Button>
-        <Link to="/upload">Upload Video</Link>
-      </Button>
+    <div>
+      <div className="flex gap-8">
+        <Input
+          id="search"
+          type="search"
+          name="search"
+          value={searchString}
+          placeholder="Search..."
+          className="max-w-sm"
+          onChange={(e) => setSearchString(e.target.value)}
+        />
+        <Button
+          type="submit"
+          className="cursor-pointer"
+          disabled={loading}
+          onClick={() => search(searchString)}
+        >
+          {loading ? <RoundSpinner /> : <Search />}
+        </Button>
+        <Button>
+          <Link to="/upload">Upload Video</Link>
+        </Button>
+      </div>
+      <div>
+        {videos.map((video) => {
+          return (
+            <div>
+              <p>Title: {video.title}</p>
+              <p>Description: {video.description}</p>
+              <p>AWSUrl: {video.awsUrl}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
