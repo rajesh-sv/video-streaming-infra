1b97f370b1fed63529a85ed2f6b1fe5626006bdf
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2025-06-05 09:37:55 +0530

    Update export

--- frontend/src/components/logout.tsx
+++ frontend/src/components/logout.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import useLogout from "@/hooks/useLogout";
+import { useLogout } from "@/hooks/useLogout";
 import { RoundSpinner } from "@/components/ui/spinner";
 
 export function Logout() {
